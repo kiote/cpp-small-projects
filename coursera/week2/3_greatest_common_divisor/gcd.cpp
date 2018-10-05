e1f5bd78b936c68c4663972d4bdaace5e1bc8fac
Ekaterina Krivich <ekaterinak@heathmont.net>
2018-10-05 09:15:09 +0300

    euclidean

--- coursera/week2/3_greatest_common_divisor/gcd.cpp
+++ coursera/week2/3_greatest_common_divisor/gcd.cpp
@@ -12,9 +12,18 @@ int gcd_naive(int a, int b) {
   return current_gcd;
 }
 
+int gcd_euclid(int a, int b) {
+  if (b == 0) {
+    return a;
+  }
+  else {
+    return gcd_euclid(b, a % b);
+  }
+}
+
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << gcd_naive(a, b) << std::endl;
+  std::cout << gcd_euclid(a, b) << std::endl;
   return 0;
 }
