d89afba5892565a6aa2a27db8d58dd43e100777f
Ekaterina Krivich <ekaterinak@heathmont.net>
2018-10-12 09:52:00 +0300

    knapsack

--- coursera/week3/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ coursera/week3/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,14 +1,57 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-using std::vector;
+using namespace std;
 
-double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
+struct Product {
+  int weight;
+  int value;
+  double price;
+};
+
+bool sortByPrice(const Product &lhs, const Product &rhs) { return lhs.price > rhs.price; };
+
+double get_optimal_value(int n, int capacity, vector<int> weights, vector<int> values) {
   double value = 0.0;
 
-  // write your code here
+  // calculate prices list
+  vector<Product> products(weights.size());
+  for (int i=0; i<weights.size(); ++i) {
+    products[i].weight = weights[i];
+    products[i].value = values[i];
+    products[i].price = (double)values[i] / weights[i];
+  }
 
-  return value;
+  // sort
+  sort(products.begin(), products.end(), sortByPrice);
+
+  //for (int i=0; i<weights.size(); ++i) {
+  //  cout << endl << "i=" << i << "; weigth=" << products[i].weight << endl;
+  //  cout << "i=" << i << "; value=" << products[i].value << endl;
+  //  cout << "i=" << i << "; price=" << products[i].price << endl;
+  //}
+
+  // put stuff to the knapsack
+  int curN = 0;
+  int curCapacity = 0;
+  int capacityLeft = capacity-curCapacity;
+  while ((curN < n) && (capacityLeft > 0)) {
+    // try to get the whole of the most expensive product
+    if (products[curN].weight <= capacityLeft) {
+      curCapacity += products[curN].weight;
+      value += (double)products[curN].value;
+      capacityLeft = capacity-curCapacity;
+    }
+    // try to get the biggest chunk if the most expensive product
+    else {
+      value += products[curN].price * (double)capacityLeft;
+      capacityLeft = 0; 
+    }
+    curN++;
+  }
+
+  return (double)value;
 }
 
 int main() {
@@ -21,7 +64,7 @@ int main() {
     std::cin >> values[i] >> weights[i];
   }
 
-  double optimal_value = get_optimal_value(capacity, weights, values);
+  double optimal_value = get_optimal_value(n, capacity, weights, values);
 
   std::cout.precision(10);
   std::cout << optimal_value << std::endl;
