95c6789a0784cb9c3eb41156ef5a0cc0022b49b6
Ekaterina Krivich <ekaterinak@heathmont.net>
2018-10-17 09:51:12 +0300

    kivi

--- kivi2018_warmaup.cpp
+++ kivi2018_warmaup.cpp
@@ -14,7 +14,7 @@ int main() {
     cin >> curNum;
     numbers.push_back(curNum);
     
-    for (int i=0; i<numbers.size()-1; ++i) {
+    for (int i=0; i<numbers.size()-2; ++i) {
         cout << numbers[i] << endl;
     }
 
